8395e0a73cbd9432df190c50646c0df9119d61a7
jaovmorais <joao.morais00@hotmail.com>
2021-06-06 21:54:16 -0300

    Commit
    Faltando somente itens 3 e 4 do menu

--- main.c
+++ main.c
@@ -6,15 +6,27 @@ void main (int argc, char *argv[ ]){
         printf("\nERRO NA ABERTURA DO ARQUIVO\n");
         return;
     }else{
-        char linha[20];
-        int maiorVertice=0, aux=0;
+        int maiorVertice=0, contador=0;
+        char leitura[500][20];
         while(!feof(file)){
-            fgets(linha, 20, file);
-            // for(int i = 0; linha[i]!= " "; i++){
-            //     aux = i-1;
-            // }
-            //printf("%d AQUI", atoi(strncpy()linha[]))
+            fgets(leitura[contador], 20, file);
+            contador++;
         }
+        int origens[contador], destinos[contador], valores[contador];
+        for(int i=0; i<contador; i++){
+            dividirString(leitura[i], &origens[i], &destinos[i], &valores[i]);
+            if(origens[i]>maiorVertice) maiorVertice = origens[i];
+            if(destinos[i]>maiorVertice) maiorVertice = destinos[i];
+        }
+        int **grafo = (int **) malloc(maiorVertice*sizeof(int));
+        for(int i = 0; i<maiorVertice; i++) grafo[i] = (int *) malloc(maiorVertice*sizeof(int));
+        zeraMatriz(grafo, maiorVertice, maiorVertice);
+        preencheMatriz(grafo, maiorVertice, origens, destinos, valores);
+        //imprimeMatriz(grafo, maiorVertice, maiorVertice);
+        int continua = 1;
+        while(continua==1) continua = menu(grafo, maiorVertice, contador);
+        for(int i =0; i<maiorVertice; i++) free(grafo[i]);
+        free(grafo);
     }
     fclose(file);
     return;
--- tp1.c
+++ tp1.c
@@ -1,7 +1,103 @@
 #include "tp1.h"
 
+void preencheMatriz(int** matriz, int tamMatriz, int* origens, int* destinos, int* valores){
+    for(int i=0; i<tamMatriz; i++){
+            matriz[origens[i]-1][destinos[i]-1] = valores[i];
+    }
+}
+
 char* obterSubstring(char* string, int inicio, int fim){
     char *newString = (char*) malloc(((fim - inicio) +1) * sizeof(char));
     strncpy(newString, string + inicio, ((fim - inicio) +1));
     return newString;
 }
+
+void dividirString(char* string, int* origem, int* destino, int* valor){
+    int espacos=0, iDestino=0, iValor=0, fimString=0;
+    for(int i=0; i<20; i++){
+        if(string[i]==' ' && espacos==0){
+            *origem = atoi(obterSubstring(string, 0, i-1));
+            iDestino = i+1;
+            espacos++;
+        }else if(string[i]==' ' && espacos==1){
+            *destino = atoi(obterSubstring(string, iDestino, i-1));
+            iValor = i+1;
+            espacos++;
+        }else if(string[i]=='\0' && espacos==2){
+            *valor = atoi(obterSubstring(string, iValor, i-1));
+        }
+    }
+}
+
+void imprimeMatriz(int** matriz, int linhas, int colunas){
+    for(int i = 0; i<linhas; i++){
+        for(int j = 0; j<colunas; j++){
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void zeraMatriz(int** matriz, int linhas, int colunas){
+    for(int i = 0; i< linhas; i++){
+        for(int j = 0; j<colunas; j++){
+            matriz[i][j] = 0;
+        }
+    }
+}
+
+int menu(int **matriz, int totalVertices, int totalArestas){
+    printf("\nMENU:\n\n1-Sumario\n2-Grau de vertice\n3-Sucessores de vertice\n4-Antecessores de vertice\n0-SAIR\n\nDigite uma opcao: ");
+    int opcao;
+    scanf("%d", &opcao);
+    switch (opcao)
+    {
+    case 1:
+        sumario(matriz, totalVertices, totalArestas);
+        return 1;
+        break;
+    case 2:
+        printf("\nDigite um vertice: ");
+        int vertice;
+        scanf("%d", &vertice);
+        grauVertice(matriz, totalVertices, vertice);
+        return 1;
+        break;
+    case 3:
+        printf("\nDigite um vertice: ");
+        int vertice;
+        scanf("%d", &vertice);
+        sucessores(matriz, totalVertices, vertice);
+        return 1;
+        break;
+    case 4:
+        printf("\nDigite um vertice: ");
+        int vertice;
+        scanf("%d", &vertice);
+        antecessores(matriz, totalVertices, vertice);
+        return 1;
+        break;
+    case 0:
+        printf("\nentrou 0\n");
+        return 0;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void grauVertice(int **matriz, int totalVertices, int vertice){
+    int entrada=0, saida=0;
+    for(int i=0; i<totalVertices; i++){
+        if(matriz[i][vertice-1]!=0) entrada++;
+        if(matriz[vertice-1][i]!=0) saida++;
+    }
+    printf("\nGrau de entrada: %d, Grau de saida: %d\n", entrada, saida);
+}
+
+void sumario(int** matriz, int totalVertices, int totalArestas){
+    float densidade = (float)totalArestas/(totalVertices*(totalVertices-1));
+    printf("Numero de Vertices: %d, Numero de Arestas: %d, Densidade: %.4f", totalVertices, totalArestas, densidade);
+}
+
